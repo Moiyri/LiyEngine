58cb263677ffef804d7dfcc6846389198307198c
Moiyri <moiyri@163.com>
2021-04-03 21:30:49 +0800

    update ULDS

--- plugins/liy_ULDS.js
+++ plugins/liy_ULDS.js
@@ -5,30 +5,24 @@
  */
 
 (() =>{
-    function Liy_ULDS(){
-    }
+    function Liy_ULDS(){}
 
-    Liy_ULDS.prototype = Object.create(Tilemap.rototype);
-    Liy_ULDS.prototype.constructor = Liy_ULDS;
+    Liy_ULDS.prototype = Object.create(Liy_ULDS.prototype);
+    Liy_ULDS.prototype.constructor = Liy_ULDS.prototype;
 
-    Liy_ULDS.protorype.obtainLayerList = function() {
-        var note = JSON.parse(this._map.note);
-        this._upperlayers = note["upperlayers"].split(",");
-        this._lowerlayers = note["lowerlayers"].split(",");
-    };
-    
+    var _Spriteset_Map_prototype_createTilemap = Spriteset_Map.prototype.createTilemap;
     Spriteset_Map.prototype.createTilemap = function() {
-        const tilemap = new Liy_ULDS();
-        tilemap.tileWidth = $gameMap.tileWidth();
-        tilemap.tileHeight = $gameMap.tileHeight();
-        tilemap.setData($gameMap.width(), $gameMap.height(), $gameMap.data());
-        tilemap.horizontalWrap = $gameMap.isLoopHorizontal();          tilemap.verticalWrap = $gameMap.isLoopVertical();
-        this._baseSprite.addChild(tilemap);
-        this._effectsContainer = tilemap;
-        this._tilemap = tilemap;
-        this.loadTileset();
+        _Spriteset_Map_prototype_createTilemap.call(this);
+        this._tiletest = new Sprite(ImageManager.loadPicture("test"));
+        this._baseSprite.addChild(this._tiletest);
+        //this._tiletest.x = -48;
+        //this._tiletest.y = -48;
     };
 
-    
-    
+    var _Spriteset_Map_prototype_updateTilemap = Spriteset_Map.prototype.updateTilemap;
+    Spriteset_Map.prototype.updateTilemap = function() {
+        _Spriteset_Map_prototype_updateTilemap.call(this);
+        this._tiletest.x = - $gameMap.displayX() * $gameMap.tileWidth();
+        this._tiletest.y = - $gameMap.displayY() * $gameMap.tileHeight();
+    };
 })();
