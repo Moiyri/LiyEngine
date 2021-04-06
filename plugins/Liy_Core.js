d65a61da01001b79cfd84c87207e6ea25245bfe6
LyFireflys <dayflowers@163.com>
2021-04-07 01:13:13 +0800

    update core.

--- plugins/Liy_Core.js
+++ plugins/Liy_Core.js
@@ -18,16 +18,18 @@
 
     const pluginName = "Liy_Core";
 
-    Liy.streamOutFile = function(path, file){
-
-    }
-
     var params = PluginManager.parameters("Liy_Core");
-    var isShowDevtools = Boolean(params["showDevtools"]) || false;
-    
+    var isShowDevtools = Boolean(params["showDevtools"]) | false;
+
+    $dataULDSMap = null;
+
+    Liy.tackleDataMap = function(){
+        var note = $dataMap.note;
+    };
+
     Main.prototype.onEffekseerLoad = function(){
         this.eraseLoadingSpinner();
         if(isShowDevtools) SceneManager.showDevTools();
         SceneManager.run(Scene_Boot);
-    }
+    };
 })();
