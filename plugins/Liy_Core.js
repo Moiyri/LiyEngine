aff726c576c364136c77fa6526a1ae700b65be5e
Moiyri <moiyri@163.com>
2021-04-17 18:50:53 +0800

    update core and ulds.

--- plugins/Liy_Core.js
+++ plugins/Liy_Core.js
@@ -33,67 +33,12 @@
     });
 
 //-----------------------------------------------------------
-
-    function Liy(){}
-
-    Liy.prototype = Object.create(Liy.prototype);
-    Liy.prototype.constructor = Liy;
-
-    var $dataULDSMap = null;
-    var $dataLiyScenes = null;
-
-    /*
-        {
-            "ulds": {
-                "layer": [{"x": 0, "y": 0, "name": 0, "location": 0}]
-            }
+    Scene_Map.prototype.onMapLoaded = function() {
+        if (this._transfer) {
+            $gamePlayer.performTransfer();
         }
-    */
-    Liy.resolveAllDataMap = function(){
-        var info = JSON.parse($dataMap.note);
-        try{
-            $dataULDSMap = info.ulds;
-        } catch(e) {
-            throw e;
-        }
-    };
-
-//-----------------------------------------------------------
-    function GlobalVar(){}
-    
-    GlobalVar.prototype = Object.create(GlobalVar.prototype);
-    GlobalVar.constructor = GlobalVar;
-
-    var $globalVar = new Map();
-
-    GlobalVar.resolveExp = function(exp){
-        exp.replace("", " ");
-        for(i in new RegExp("/(=)|(+=)|(-=)|(*=)|(/=).?(/w)/").exec(exp){
-            exp.replace(i, $globalVar[i]);
-        }
-    };
-
-    GlobalVar.execExp = function(exp){
-        
-    }
-
-    GlobalVar.VarFileStream = function(isload){
-        if(isload) $globalVariable = StorageManager.loadObject("GlobalVariable")
-        else StorageManager.saveObject("GlobalVariable", $globalVariable);
-    };
-
-    GlobalVar.operatVar = function(op) {
-        
-    };
-
-//-----------------------------------------------------------
-    function Lib_LibBezier(){}
-
-//-----------------------------------------------------------
-    var _Scene_Map_prototype_onMapLoaded = Scene_Map.prototype.onMapLoaded;
-    Scene_Map.prototype.onMapLoaded = function(){
-        _Scene_Map_prototype_onMapLoaded.call(this);
         Liy.resolveAllDataMap();
+        this.createDisplayObjects();
     };
 
     Main.prototype.onEffekseerLoad = function(){
@@ -102,3 +47,47 @@
         SceneManager.run(Scene_Boot);
     };
 })();
+
+//-----------------------------------------------------------
+function Liy() {}
+
+var $dataULDSMap = null;
+var $dataLiyScenes = null;
+
+Liy.resolveAllDataMap = function() {
+    var info = JSON.parse($dataMap.note);
+    try{
+        $dataULDSMap = info.ulds;
+    } catch(e) {
+        throw e;
+    }
+};
+
+//-----------------------------------------------------------
+function Lib_LibBezier(){}
+
+//-----------------------------------------------------------
+function GlobalVar() {}
+    
+GlobalVar.prototype = Object.create(GlobalVar.prototype);
+GlobalVar.constructor = GlobalVar;
+
+var $globalVar = new Map();
+
+GlobalVar.resolveExp = function(exp){
+exp.replace("", " ");
+    for(i in new RegExp("/(=)|(+=)|(-=)|(*=)|(/=).?(/w)/").exec(exp)){
+        exp.replace(i, $globalVar[i]);
+    }
+};
+
+GlobalVar.execExp = function(exp){
+}
+
+GlobalVar.VarFileStream = function(isload){
+    if(isload) $globalVariable = StorageManager.loadObject("GlobalVariable")
+    else StorageManager.saveObject("GlobalVariable", $globalVariable);
+};
+
+GlobalVar.operatVar = function(op) {
+};
--- plugins/Liy_ULDS.js
+++ plugins/Liy_ULDS.js
@@ -2,11 +2,23 @@
  * @target MZ
  * @plugindesc An unlimited layer display system belongs to LiyEngine.
  * @author Moiyri
+ * 
+ * @help Liy_ULDS.js
+ * 
+ * Usage:
+ *  To write note like this:
+ * 
+ * {
+ *  "ulds": {
+ *      "layer": [{"x": 0, "y": 0, "name": 0, "z": 0, "tiling" : 0, "blendmode" : 0}, 
+ *      {"x": 0, "y": 0, "name": 0, "z": 0, "tiling" : 0, "blendmode" : 0}]
+ *  }
+ * }
+ * 
  */
 
 (() =>{
-    function Liy_ULDS(){
-    }
+    function Liy_ULDS(){}
 
     Liy_ULDS.prototype = Object.create(Liy_ULDS.prototype);
     Liy_ULDS.prototype.constructor = Liy_ULDS.prototype;  
@@ -14,17 +26,26 @@
     var _Spriteset_Map_prototype_createTilemap = Spriteset_Map.prototype.createTilemap;
     Spriteset_Map.prototype.createTilemap = function() {
         _Spriteset_Map_prototype_createTilemap.call(this);
-        for(var i = 0; i < $dataULDSMap.layer.length; i++){
-            this._uldsSprite = new Sprite(ImageManager.loadPicture($dataULDSMap.layer[i].name));
+        this._uldsSprite = [];
+        for(var i = 0, sprite = null; i < $dataULDSMap.layer.length; i++){
+            sprite = $dataULDSMap.layer[i].tiling | false ? new TilingSprite() : new Sprite();
+            sprite.bitmap = ImageManager.loadPicture($dataULDSMap.layer[i].name);
+            sprite.x = $dataULDSMap.layer[i].x;
+            sprite.y = $dataULDSMap.layer[i].y;
+            sprite.z = $dataULDSMap.layer[i].z;
+            sprite.blendMode = $dataULDSMap.layer[i].blendmode;
+            sprite.move(sprite.x, sprite.y, Graphics.width, Graphics.height)
+            this._uldsSprite.push(sprite);
+            this.addChild(sprite);
         }
     };
 
     var _Spriteset_Map_prototype_updateTilemap = Spriteset_Map.prototype.updateTilemap;
     Spriteset_Map.prototype.updateTilemap = function() {
         _Spriteset_Map_prototype_updateTilemap.call(this);
-        for(var i = 0; i < this._ULDSLayer.length; i++){
-            this._ULDSLayer[i].sprite.x = this._ULDSLayer[i].x - $gameMap.displayX() *  $gameMap.tileWidth(); 
-            this._ULDSLayer[i].sprite.y = this._ULDSLayer[i].y - $gameMap.displayY() * $gameMap.tileHeight();
+        for(var i = 0; i < $dataULDSMap.layer.length; i++){
+            this._uldsSprite[i].x = $dataULDSMap.layer[i].x - $gameMap.displayX() *  $gameMap.tileWidth(); 
+            this._uldsSprite[i].y = $dataULDSMap.layer[i].y - $gameMap.displayY() * $gameMap.tileHeight();
         }
     };
 })();
